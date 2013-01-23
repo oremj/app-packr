8d0a677ed1ed5939d9a6d4e699c94db0dda76869
Jeremiah Orem <oremj@oremj.com>
2013-01-23 14:26:11 -0800

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='mozdeploy',
-    version='0.0.1',
+    version='0.0.2',
     description='Deploy tools',
     author='Jeremy Orem',
     author_email='oremj@mozilla.com',
