a73e97c929875c0ee612a7fc9bab649cfd4f5cba
Jeremiah Orem <oremj@oremj.com>
2013-01-23 17:47:55 -0800

    More tests.

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,5 +1,12 @@
+import os
+from shutil import rmtree
+from tempfile import mkdtemp
+
 from mozdeploy import build
 
+
+ref = '1a2b3c4d5e6f'
+build_id = build.get_build_id(ref)
 
 def test_get_clean_version():
     v = build.get_clean_version('abcABC123-.!@#$')
@@ -7,9 +14,6 @@ def test_get_clean_version():
 
 
 def test_get_build_id():
-    ref = '1a2b3c4d5e6f'
-    build_id = build.get_build_id(ref)
-
     ts, ref_ = build_id.split('-')
     
     assert ref_ == ref[:10]
@@ -17,15 +21,37 @@ def test_get_build_id():
 
 
 def test_build_app():
-    pass
+    build_dir = mkdtemp()
+    build.build_app('echo test_build_app > testfile', ref, build_id, build_dir)
 
+    assert "test_build_app\n" == open(os.path.join(build_dir,
+                                                   build_id,
+                                                   'testfile')).read()
+
+    rmtree(build_dir)
+    
 
 def test_compress_dir():
-    pass
+    build_dir = mkdtemp()
+    host_dir = mkdtemp()
+
+    build.build_app('true', ref, build_id, build_dir)
+    build.compress_dir(host_dir, build_dir, build_id)
+
+    assert os.path.isfile(os.path.join(host_dir, build_id, 'release.tar'))
+
+    rmtree(build_dir)
+    rmtree(host_dir)
 
 
 def test_set_latest():
-    pass
+    host_dir = mkdtemp()
+
+    build.set_latest(host_dir, build_id)
+
+    assert build_id == open(os.path.join(host_dir, 'LATEST')).read()
+
+    rmtree(host_dir)
 
 
 def test_cleanup():
